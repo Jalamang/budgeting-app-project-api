93f3ac88fd81d466bb152ca151443b99629595da
Jalamang <Jalamangjanka@gmail.com>
2022-01-21 15:58:29 -0500

    package.json Budget-App-API

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 // Configuration
 dotenv.config()
 
-const PORT = process.env.API_URL || 3000;
+const PORT = process.env.PORT || 3000;
 
 index.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 
